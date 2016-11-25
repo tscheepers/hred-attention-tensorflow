494fa33e55940444a7dc5e5d3e60f78d6c6dbd48
Thijs Scheepers <thijs@label305.com>
2016-11-25 20:11:25 +0100

    Batch works

--- src/hred/hred.py
+++ src/hred/hred.py
@@ -9,19 +9,19 @@ class HRED():
     def __init__(self):
         # We do not need to define parameters explicitly since tf.get_variable() also creates parameters for us
 
-        self.vocab_size = 20
-        self.embedding_size = 5
-        self.query_hidden_size = 10
-        self.session_hidden_size = 15
+        self.vocab_size = 6
+        self.embedding_size = 3
+        self.query_hidden_size = 4
+        self.session_hidden_size = 5
         self.decoder_hidden_size = self.query_hidden_size
         self.eoq_symbol = 0  # End of Query symbol
 
-    def step(self, X):
+    def step(self, X, start_hidden_query, start_hidden_session, start_hidden_decoder, start_output):
 
         embedder = layers.embedding_layer(X, vocab_dim=self.vocab_size, embedding_dim=self.embedding_size)
 
         # Mask used to reset the query encoder when symbol is End-Of-Query symbol
-        x_mask = tf.cast(tf.not_equal(X, self.eoq_symbol), tf.float32)
+        x_mask = tf.expand_dims(tf.cast(tf.not_equal(X, self.eoq_symbol), tf.float32), 2)
 
         query_encoder_packed = tf.scan(
             lambda result_prev, x: layers.gru_layer_with_reset(
@@ -32,7 +32,7 @@ class HRED():
                 y_dim=self.query_hidden_size
             ),
             (embedder, x_mask),  # scan does not accept multiple tensors so we need to pack and unpack
-            initializer=tf.zeros((2, self.query_hidden_size))
+            initializer=start_hidden_query
         )
 
         query_encoder, _ = tf.unpack(query_encoder_packed, axis=1)
@@ -46,7 +46,7 @@ class HRED():
                 y_dim=self.session_hidden_size
             ),
             (query_encoder, x_mask),
-            initializer=tf.zeros((2, self.session_hidden_size))
+            initializer=start_hidden_session
         )
 
         session_encoder, _ = tf.unpack(session_encoder_packed, axis=1)
@@ -61,21 +61,32 @@ class HRED():
                 y_dim=self.decoder_hidden_size
             ),
             (embedder, x_mask, session_encoder),  # scan does not accept multiple tensors so we need to pack and unpack
-            initializer=tf.zeros((self.decoder_hidden_size, ))
+            initializer=start_hidden_decoder
         )
 
-        output = layers.output_layer(
-            decoder,
-            embedder,
-            x_dim=self.embedding_size,
-            h_dim=self.decoder_hidden_size,
-            y_dim=self.decoder_hidden_size
+        def output_func(_, x_packed):
+            decoder, embedder, session_encoder = x_packed
+
+            output = layers.output_layer(
+                decoder,
+                embedder,
+                session_encoder,
+                x_dim=self.embedding_size,
+                h_dim=self.decoder_hidden_size,
+                y_dim=self.decoder_hidden_size,
+                s_dim=self.session_hidden_size
+            )
+
+            return layers.softmax_layer(
+                output,
+                x_dim=self.decoder_hidden_size,
+                y_dim=self.vocab_size
+            )
+
+        output = tf.scan(
+            output_func,
+            (decoder, embedder, session_encoder),
+            initializer=start_output
         )
 
-        softmax = layers.softmax_layer(
-            output,
-            x_dim=self.decoder_hidden_size,
-            y_dim=self.vocab_size
-        )
-
-        return softmax, output, decoder, session_encoder, query_encoder
+        return output
--- src/hred/train.py
+++ src/hred/train.py
@@ -8,20 +8,36 @@ if __name__ == '__main__':
 
         hred = HRED()
         batch_size = None
+        max_length = None
 
-        X = tf.placeholder(tf.int32, shape=(batch_size, 1))
-        graph = hred.step(X)
+        X = tf.placeholder(tf.int32, shape=(max_length, batch_size))
+        HQ0 = tf.placeholder(tf.float32, (2, batch_size, hred.query_hidden_size))
+        HS0 = tf.placeholder(tf.float32, (2, batch_size, hred.session_hidden_size))
+        HD0 = tf.placeholder(tf.float32, (batch_size, hred.decoder_hidden_size))
+        O0 = tf.placeholder(tf.float32, (batch_size, hred.vocab_size))
+
+        graph = hred.step(X, HQ0, HS0, HD0, O0)
 
         with tf.Session() as sess:
 
             sess.run(tf.initialize_all_variables())
 
+            batch_size = 10
+            max_length = 5
+
             summary_writer = tf.train.SummaryWriter('logs/graph', sess.graph)
 
-            x = np.random.randint(0, hred.vocab_size, (5, 1))
+            x = np.random.randint(0, hred.vocab_size, (max_length, batch_size))
+            hq0 = np.zeros((2, batch_size, hred.query_hidden_size))
+            hs0 = np.zeros((2, batch_size, hred.session_hidden_size))
+            hd0 = np.zeros((batch_size, hred.decoder_hidden_size))
+            o0 = np.zeros((batch_size, hred.vocab_size))
 
             print(x)
 
-            softmax, output, decoder, session_encoder, query_encoder = sess.run(graph, {X: x})
+            softmax = sess.run(
+                graph,
+                {X: x, HQ0: hq0, HS0: hs0, HD0: hd0, O0: o0}
+            )
 
             print(softmax)
