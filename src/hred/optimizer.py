bfcdf03268477e049157d5947650218c82382c71
Thijs Scheepers <thijs@label305.com>
2016-11-29 17:24:50 +0100

    Added sampling during training

--- src/hred/optimizer.py
+++ src/hred/optimizer.py
@@ -28,11 +28,14 @@ class Optimizer(object):
 
         self.global_step = tf.Variable(0, trainable=False, dtype=tf.int32)
 
-        learning_rate = tf.train.exponential_decay(
-            initial_learning_rate, self.global_step, num_steps_per_decay,
-            decay_rate, staircase=True)
+        # learning_rate = tf.train.exponential_decay(
+        #     initial_learning_rate, self.global_step, num_steps_per_decay,
+        #     decay_rate, staircase=True)
+        #
+        # optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate, beta1=0.1, beta2=0.001)
 
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.1, beta2=0.001)
+        optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0001, decay=0.95, momentum=0.9, epsilon=1e-6)
+
         self._optimize_op = optimizer.apply_gradients(grad_var_pairs, global_step=self.global_step)
 
     @property
