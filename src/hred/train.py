671ac7ebeb286a83b82ba0192ff9e9dc06346ac3
maartjeth <maartjeterhoeve@gmail.com>
2016-11-29 16:41:40 +0100

    tensorboard stuff

--- src/hred/train.py
+++ src/hred/train.py
@@ -15,6 +15,7 @@ TRAIN_FILE = '../../data/aol_sess_50000.out'
 VALIDATION_FILE = '../../data/val_session.out'
 TEST_FILE = '../../data/test_session.out'
 SMALL_FILE = '../../data/small_train.out'
+TRAIN_DIR = 'logs'
 
 CHECKPOINT_FILE = '../../checkpoints/model.ckpt'
 
@@ -38,19 +39,22 @@ if __name__ == '__main__':
         non_symbol_accuracy = hred.non_symbol_accuracy(logits, Y)
 
         optimizer = Optimizer(loss, initial_learning_rate=0.0002, num_steps_per_decay=1000,
-                      decay_rate=0.5, max_global_norm=1.0)
+                              decay_rate=0.5, max_global_norm=1.0)
+
+        summary = tf.merge_all_summaries()
 
         # Add an op to initialize the variables.
         init_op = tf.initialize_all_variables()
 
         # Add ops to save and restore all the variables.
+
         saver = tf.train.Saver()
 
         with tf.Session() as sess:
 
             sess.run(init_op)
+            summary_writer = tf.train.SummaryWriter(TRAIN_DIR, sess.graph)
 
-            # summary_writer = tf.train.SummaryWriter('logs/graph', sess.graph)
 
             batch_size = 100
             max_length = 20
@@ -81,6 +85,13 @@ if __name__ == '__main__':
                     )
 
                     if iteration % 10 == 0:
+                        summary_str = sess.run(summary, hred.populate_feed_dict_with_defaults(
+                            batch_size=batch_size,
+                            feed_dict={X: x_batch, Y: y_batch}
+                        ))
+                        summary_writer.add_summary(summary_str, iteration)
+                        summary_writer.flush()
+
                         print("Step %d - Loss: %f   Acc: %f   Acc (no symbols): %f" % (iteration, loss_out, acc_out, accuracy_non_special_symbols_out))
 
                     if iteration % 1000 == 0:
@@ -90,6 +101,7 @@ if __name__ == '__main__':
                         if not math.isnan(loss_out):
                             # Save the variables to disk.
                             save_path = saver.save(sess, CHECKPOINT_FILE)
+
                             print("Model saved in file: %s" % save_path)
 
                     iteration += 1
