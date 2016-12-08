3845fd845bc52d7c7b8b02ce1dfda229e7807d69
Thijs Scheepers <thijs@label305.com>
2016-12-08 18:04:07 +0100

    fixed beam search with new data

--- src/hred/train.py
+++ src/hred/train.py
@@ -44,8 +44,11 @@ SEED = 1234
 class Trainer(object):
 
     def __init__(self):
-        self.vocab_lookup_dict = cPickle.load(open("../../data/allesandro/all/train.dict.pkl", 'r'))
-        self.train_data, _ = allesandro_data_iterator.get_batch_iterator(np.random.RandomState(SEED), {
+
+        vocab = cPickle.load(open("../../data/allesandro/all/train.dict.pkl", 'r'))
+        self.vocab_lookup_dict = {k: v for v, k, count in vocab}
+
+        self.train_data, self.valid_data = allesandro_data_iterator.get_batch_iterator(np.random.RandomState(SEED), {
             'eoq_sym': 1,
             'eos_sym': 2,
             'sort_k_batches': 20,
@@ -57,6 +60,7 @@ class Trainer(object):
             'valid_rank': "../../data/allesandro/all/valid.rnk.pkl"
         })
         self.train_data.start()
+        self.valid_data.start()
 
         vocab_size = len(self.vocab_lookup_dict)
         # vocab_size = VOCAB_SIZE
@@ -71,10 +75,10 @@ class Trainer(object):
         self.X = tf.placeholder(tf.int64, shape=(max_length, batch_size))
         self.Y = tf.placeholder(tf.int64, shape=(max_length, batch_size))
 
-        # self.X_sample = tf.placeholder(tf.int64, shape=(batch_size,))
-        # self.H_query = tf.placeholder(tf.float32, shape=(batch_size, self.hred.query_dim))
-        # self.H_session = tf.placeholder(tf.float32, shape=(batch_size, self.hred.session_dim))
-        # self.H_decoder = tf.placeholder(tf.float32, shape=(batch_size, self.hred.decoder_dim))
+        self.X_sample = tf.placeholder(tf.int64, shape=(batch_size,))
+        self.H_query = tf.placeholder(tf.float32, shape=(batch_size, self.hred.query_dim))
+        self.H_session = tf.placeholder(tf.float32, shape=(batch_size, self.hred.session_dim))
+        self.H_decoder = tf.placeholder(tf.float32, shape=(batch_size, self.hred.decoder_dim))
 
         self.logits = self.hred.step_through_session(self.X)
         self.loss = self.hred.loss(self.X, self.logits, self.Y)
@@ -82,10 +86,10 @@ class Trainer(object):
         self.accuracy = self.hred.non_padding_accuracy(self.logits, self.Y)
         self.non_symbol_accuracy = self.hred.non_symbol_accuracy(self.logits, self.Y)
 
-        # self.session_inference = self.hred.step_through_session(self.X, return_softmax=True,
-        #                                                         return_last_with_hidden_states=True, reuse=True)
-        # self.step_inference = self.hred.single_step(self.X_sample, self.H_query, self.H_session, self.H_decoder,
-        #                                             reuse=True)
+        self.session_inference = self.hred.step_through_session(self.X, return_softmax=True,
+                                                                return_last_with_hidden_states=True, reuse=True)
+        self.step_inference = self.hred.single_step(self.X_sample, self.H_query, self.H_session, self.H_decoder,
+                                                    reuse=True)
 
         self.optimizer = Optimizer(self.loss)
         self.summary = tf.merge_all_summaries()
@@ -110,25 +114,35 @@ class Trainer(object):
 
             for iteration in range(10000):
 
-                data = self.train_data.next()
+                x_batch, y_batch, seq_len = self.get_batch(self.train_data)
 
-                # The training is done with a trick. We append a special </q> at the beginning of the dialog
-                # so that we can predict also the first sent in the dialog starting from the dialog beginning token (</q>).
-                seq_len = data['max_length']
-                prepend = np.ones((1, data['x'].shape[1]))
-                x_data_full = np.concatenate((prepend, data['x']))
-                x_batch = x_data_full[:seq_len]
-                y_batch = x_data_full[1:seq_len + 1]
-
-                # x_batch = np.transpose(np.asarray(x_batch))
-                # y_batch = np.transpose(np.asarray(y_batch))
-
-                loss_out, _, acc_out, accuracy_non_special_symbols_out = tf_sess.run(
-                    [self.loss, self.optimizer.optimize_op, self.accuracy, self.non_symbol_accuracy],
-                    self.hred.populate_feed_dict_with_defaults(
-                        batch_size=batch_size, feed_dict={self.X: x_batch, self.Y: y_batch}
+                if iteration % 10 == 0:
+                    loss_out, _, acc_out, accuracy_non_special_symbols_out = tf_sess.run(
+                        [self.loss, self.optimizer.optimize_op, self.accuracy, self.non_symbol_accuracy],
+                        self.hred.populate_feed_dict_with_defaults(
+                            batch_size=batch_size, feed_dict={self.X: x_batch, self.Y: y_batch}
+                        )
                     )
-                )
+
+                    # Accumulative cost, like in hred-qs
+                    total_loss += loss_out
+                    n_pred += seq_len * batch_size
+                    cost = total_loss / n_pred
+
+                    print("Step %d - Cost: %f   Loss: %f   Accuracy: %f   Accuracy (no symbols): %f  Length: %d" %
+                      (iteration, cost, loss_out, acc_out, accuracy_non_special_symbols_out, seq_len))
+
+                else:
+                    loss_out, _ = tf_sess.run(
+                        [self.loss, self.optimizer.optimize_op],
+                        self.hred.populate_feed_dict_with_defaults(
+                            batch_size=batch_size, feed_dict={self.X: x_batch, self.Y: y_batch}
+                        )
+                    )
+
+                    # Accumulative cost, like in hred-qs
+                    total_loss += loss_out
+                    n_pred += seq_len * batch_size
 
                 # Sumerize
                 if iteration % 100 == 0:
@@ -138,60 +152,82 @@ class Trainer(object):
                     summary_writer.add_summary(summary_str, iteration)
                     summary_writer.flush()
 
-                # Accumulative cost, like in hred-qs
-                total_loss += loss_out
-                n_pred += seq_len * batch_size
-                cost = total_loss / n_pred
-
-                if iteration % 10 == 0:
-                    print("Step %d - Cost: %f   Loss: %f   Accuracy: %f   Accuracy (no symbols): %f  Length: %d" %
-                      (iteration, cost, loss_out, acc_out, accuracy_non_special_symbols_out, seq_len))
-
                 if iteration % 100 == 0:
                     self.save_model(tf_sess, loss_out)
                     self.sample(tf_sess)
 
                 iteration += 1
 
-    def sample(self, sess, max_sample_length=30):
+    def sample(self, sess, max_sample_length=30, num_of_samples=3):
 
-        # for (x, _) in read_data.read_line(SAMPLE_FILE):
-        #
-        #     input_x = np.expand_dims(np.asarray(x), 1)
-        #
-        #     softmax_out, hidden_query, hidden_session, hidden_decoder = sess.run(
-        #         self.session_inference,
-        #         self.hred.populate_feed_dict_with_defaults(
-        #             batch_size=1, feed_dict={self.X: input_x}
-        #         )
-        #     )
-        #
-        #     x = np.argmax(softmax_out, axis=1)
-        #     result = [x]
-        #     i = 0
-        #
-        #     while x != self.hred.eos_symbol and i < max_sample_length:
-        #         softmax_out, hidden_query, hidden_session, hidden_decoder = sess.run(
-        #             self.step_inference,
-        #             {self.X_sample: x, self.H_query: hidden_query, self.H_session: hidden_session,
-        #              self.H_decoder: hidden_decoder}
-        #         )
-        #
-        #         x = np.argmax(softmax_out, axis=1)
-        #         result += [x]
-        #         i += 1
-        #
-        #     input_x = np.array(input_x).flatten()
-        #     result = np.array(result).flatten()
-        #     print('Sample input: %s' % (' '.join([self.vocab_lookup_dict.get(x, "NOT_FOUND") for x in input_x]),))
-        #     print('Sample output words: %s' % (' '.join([self.vocab_lookup_dict.get(x, "NOT_FOUND") for x in result])))
-        pass
+        for i in range(num_of_samples):
+
+            x_batch, _, seq_len = self.get_batch(self.valid_data)
+            input_x = np.expand_dims(x_batch[:-(seq_len / 2), 1], axis=1)
+
+            softmax_out, hidden_query, hidden_session, hidden_decoder = sess.run(
+                self.session_inference,
+                self.hred.populate_feed_dict_with_defaults(
+                    batch_size=1, feed_dict={self.X: input_x}
+                )
+            )
+
+            # Ignore UNK
+            arg_sort = np.argsort(softmax_out, axis=1)
+            if arg_sort[0, 0] == self.hred.unk_symbol:
+                x = arg_sort[:, 1]
+            else:
+                x = arg_sort[:, 0]
+
+            result = [x]
+            i = 0
+
+            while x != self.hred.eos_symbol and i < max_sample_length:
+                softmax_out, hidden_query, hidden_session, hidden_decoder = sess.run(
+                    self.step_inference,
+                    {self.X_sample: x, self.H_query: hidden_query, self.H_session: hidden_session,
+                     self.H_decoder: hidden_decoder}
+                )
+
+                # Ignore UNK
+                arg_sort = np.argsort(softmax_out, axis=1)
+                if arg_sort[0, 0] == self.hred.unk_symbol:
+                    x = arg_sort[:, 1]
+                else:
+                    x = arg_sort[:, 0]
+
+                result += [x]
+                i += 1
+
+            input_x = np.array(input_x).flatten()
+            result = np.array(result).flatten()
+            print('Sample input: %s' % (' '.join([self.vocab_lookup_dict.get(x, "NOT_FOUND") for x in input_x]),))
+            print('Sample output words: %s' % (' '.join([self.vocab_lookup_dict.get(x, "NOT_FOUND") for x in result])))
+
+
 
     def save_model(self, sess, loss_out):
         if not math.isnan(loss_out):
             # Save the variables to disk.
             save_path = self.saver.save(sess, CHECKPOINT_FILE)
             print("Model saved in file: %s" % save_path)
+
+    def get_batch(self, train_data):
+
+        # The training is done with a trick. We append a special </q> at the beginning of the dialog
+        # so that we can predict also the first sent in the dialog starting from the dialog beginning token (</q>).
+
+        data = train_data.next()
+        seq_len = data['max_length']
+        prepend = np.ones((1, data['x'].shape[1]))
+        x_data_full = np.concatenate((prepend, data['x']))
+        x_batch = x_data_full[:seq_len]
+        y_batch = x_data_full[1:seq_len + 1]
+
+        # x_batch = np.transpose(np.asarray(x_batch))
+        # y_batch = np.transpose(np.asarray(y_batch))
+
+        return x_batch, y_batch, seq_len
 
 
 if __name__ == '__main__':
